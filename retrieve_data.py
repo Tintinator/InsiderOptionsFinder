7b9943a70e708102d577c4eb1c4753d0e24173e4
Tin La <tinsila1@gmail.com>
2020-09-15 16:38:25 -0500

    retrieved day, sec filing, and company ticker. retrieve_data created

---
+++ retrieve_data.py
@@ -0,0 +1,8 @@
+import time
+
+def getCompanyTickers():
+    nasdaqListed = 'http://ftp.nasdaqtrader.com/dynamic/SymDir/nasdaqlisted.txt'
+    otherListed = 'http://ftp.nasdaqtrader.com/dynamic/SymDir/otherlisted.txt'
+
+    
+    exit()
