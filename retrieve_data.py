e660c0532b5e02c2d25d9be615656e3020720aa1
Tin La <tinsila1@gmail.com>
2020-09-15 16:39:30 -0500

    Revert "retrieved day, sec filing, and company ticker. retrieve_data created"
    Don't include env
    
    This reverts commit 7b9943a70e708102d577c4eb1c4753d0e24173e4.

--- retrieve_data.py
+++
@@ -1,8 +0,0 @@
-import time
-
-def getCompanyTickers():
-    nasdaqListed = 'http://ftp.nasdaqtrader.com/dynamic/SymDir/nasdaqlisted.txt'
-    otherListed = 'http://ftp.nasdaqtrader.com/dynamic/SymDir/otherlisted.txt'
-
-    
-    exit()
